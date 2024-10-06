b0fcdaae34bf29e4e7bdc9a2d77d26b5544c510d
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 21:24:23 +0300

    feat: from the todolist component, the deletetodo property is passed as a property to the todo component.also added the index property (Sun, Oct 6, 2024, 9:24:11 PM)

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,14 +1,14 @@
 import { Todo } from "./Todo"
 import styles from "./TodoList.module.scss"
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ deleteTodo, todos }) => {
 	return (
 		<div className={styles["todoList-container"]}>
 			{!todos.length && (
 				<h2 className={styles.todoList__header}>TodoList is empty</h2>
 			)}
 			{todos.map((todo, i) => (
-				<Todo todo={todo} key={i} />
+				<Todo deleteTodo={deleteTodo} index={i} todo={todo} key={i} />
 			))}
 		</div>
 	)
