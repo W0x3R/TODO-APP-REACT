8dbdaff93237fe986950511a39470ef9aa680d48
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 15:46:23 +0300

    feat: the header class has been added to the h1 tag in the app component.  (Sun, Oct 6, 2024, 3:46:19 PM)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>Todo APP</h1>
+			<h1 className="header">Todo APP</h1>
 			<TodoForm addTodo={addTodoHandler} />
 			<TodoList todos={todos} />
 		</div>
