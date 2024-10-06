42c5091a26dd5e0af6de6bb3e9f32581aec4f635
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 21:22:04 +0300

    feat: added deletedotohandlers function, which is added as a property for the todoform component (Sun, Oct 6, 2024, 9:21:56 PM)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ function App() {
 		setTodos([...todos, text])
 	}
 
+	const deleteTodoHandler = (index) => {
+		setTodos(todos.filter((_, i) => i !== index))
+	}
+
 	return (
 		<div className="App">
-			<TodoForm todos={todos} addTodo={addTodoHandler}>
+			<TodoForm
+				todos={todos}
+				addTodo={addTodoHandler}
+				deleteTodo={deleteTodoHandler}
+			>
 				<h1 className="header">Todo APP</h1>
 			</TodoForm>
 		</div>
