b00f733e16068f054daed861581c569013bf1edf
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 21:35:35 +0300

    feat: imported and used ritodofill icon.added todo__icon class to it.added styles to it. (Sun, Oct 6, 2024, 9:35:32 PM)

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import { RiTodoFill } from "react-icons/ri"
 import styles from "./Todo.module.scss"
 
 export const Todo = ({ deleteTodo, todo, index }) => {
 	return (
 		<div onDoubleClick={() => deleteTodo(index)} className={styles.todo}>
+			<RiTodoFill className={styles.todo__icon} />
 			<div className={styles.todo__text}>{todo}</div>
 		</div>
 	)
