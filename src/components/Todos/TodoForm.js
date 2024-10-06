7f730b1cc9de5a9c55dc83558ddb0b6634e50417
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 15:40:58 +0300

    feat: a state has been created in the todoform component to control the value of the input.  the onchangehandler function was created to set the value for the text variable, which is used during the onchange event in the input. created onsubmithandler function that is used during onsubmit event to add a new value to the to-do array. (Sun, Oct 6, 2024, 3:40:54 PM)

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,7 +1,23 @@
-export const TodoForm = () => {
+import { useState } from "react"
+
+export const TodoForm = ({ addTodo }) => {
+	const [text, setText] = useState("")
+
+	const onChangeHandler = (e) => setText(e.target.value)
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault()
+		addTodo(text)
+		setText("")
+	}
+
 	return (
-		<form className="form">
-			<input placeholder="Enter new todo"></input>
+		<form onSubmit={onSubmitHandler} className="form">
+			<input
+				value={text}
+				onChange={onChangeHandler}
+				placeholder="Enter new todo"
+			></input>
 			<button type="submit">Submit</button>
 		</form>
 	)
