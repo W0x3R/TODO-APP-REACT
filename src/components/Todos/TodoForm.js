45a96a7f2020a172860843f218a3748aae05d6ed
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 18:00:57 +0300

    feat: added styles to the todoform.js file (Sun, Oct 6, 2024, 6:00:52 PM)

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
+import styles from "./TodoForm.module.scss"
+import { TodoList } from "./TodoList"
 
-export const TodoForm = ({ addTodo }) => {
+export const TodoForm = ({ todos, addTodo, children }) => {
 	const [text, setText] = useState("")
 
 	const onChangeHandler = (e) => setText(e.target.value)
@@ -12,13 +14,20 @@ export const TodoForm = ({ addTodo }) => {
 	}
 
 	return (
-		<form onSubmit={onSubmitHandler} className="form">
-			<input
-				value={text}
-				onChange={onChangeHandler}
-				placeholder="Enter new todo"
-			></input>
-			<button type="submit">Submit</button>
-		</form>
+		<div className={styles["form-container"]}>
+			{children}
+			<form onSubmit={onSubmitHandler} className={styles.form}>
+				<input
+					className={styles.form__input}
+					value={text}
+					onChange={onChangeHandler}
+					placeholder="Enter new todo"
+				></input>
+				<button className={styles.form__btn} type="submit">
+					Submit
+				</button>
+			</form>
+			<TodoList todos={todos} />
+		</div>
 	)
 }
