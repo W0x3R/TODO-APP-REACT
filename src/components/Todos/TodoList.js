8b7f9420adf3f4911ddde0b3aead578a6aaddd37
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-06 20:52:56 +0300

    feat: added check for empty array in todolist component. added classes and styles to them. (Sun, Oct 6, 2024, 8:53:37 PM)

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,11 +1,15 @@
 import { Todo } from "./Todo"
+import styles from "./TodoList.module.scss"
 
 export const TodoList = ({ todos }) => {
 	return (
-		<>
+		<div className={styles["todoList-container"]}>
+			{!todos.length && (
+				<h2 className={styles.todoList__header}>TodoList is empty</h2>
+			)}
 			{todos.map((todo, i) => (
 				<Todo todo={todo} key={i} />
 			))}
-		</>
+		</div>
 	)
 }
