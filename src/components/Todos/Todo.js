4e6f7e564b78ef7a744abbdcf8abb721dd7f0bc3
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-08 20:33:37 +0300

    feat: connected and added icons, added classes (Tue, Oct 8, 2024, 8:33:34 PM)

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,11 +1,24 @@
-import { RiTodoFill } from "react-icons/ri"
 import styles from "./Todo.module.scss"
+import { RiTodoFill, RiDeleteBin2Line } from "react-icons/ri"
+import { FaCheck } from "react-icons/fa"
 
-export const Todo = ({ deleteTodo, todo, index }) => {
+export const Todo = ({ deleteTodo, toggleTodo, todo }) => {
 	return (
-		<div onDoubleClick={() => deleteTodo(index)} className={styles.todo}>
+		<div
+			className={`${styles.todo} ${
+				todo.isCompleted ? styles.todo_completed : ""
+			}`}
+		>
 			<RiTodoFill className={styles.todo__icon} />
-			<div className={styles.todo__text}>{todo}</div>
+			<div className={styles.todo__text}>{todo.text}</div>
+			<RiDeleteBin2Line
+				onClick={() => deleteTodo(todo.id)}
+				className={styles["todo__delete-icon"]}
+			/>
+			<FaCheck
+				onClick={() => toggleTodo(todo.id)}
+				className={styles["todo__check-icon"]}
+			/>
 		</div>
 	)
 }
