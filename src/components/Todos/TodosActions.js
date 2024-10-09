0a6f1d9e20ffdf8cf8357f5edf55cb91ef6c9d0a
Stanislau Vyrski <vyr4376@yandex.ru>
2024-10-09 16:45:47 +0300

    feat: the completedtodoslength property from the todosactions component is passed to the button component as a property (Wed, Oct 9, 2024, 4:45:41 PM)

--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -2,13 +2,17 @@ import { Button } from "../UI/Button"
 import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri"
 import styles from "./TodosActions.module.scss"
 
-export const TodosActions = ({ resetTodo, resetCompetedTodo }) => {
+export const TodosActions = ({
+	resetTodo,
+	resetCompetedTodo,
+	completedTodosLength
+}) => {
 	return (
 		<div className={styles.actions__wrapper}>
 			<Button onClick={resetTodo}>
 				<RiRefreshLine title="Reset todos" />
 			</Button>
-			<Button onClick={resetCompetedTodo}>
+			<Button disabled={!completedTodosLength} onClick={resetCompetedTodo}>
 				<RiDeleteBin2Line title="Clear completed todos" />
 			</Button>
 		</div>
